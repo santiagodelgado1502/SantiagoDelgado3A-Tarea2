644783aac5a4a2397b1b0cf41bf861cf31d760a3
santiagodelgado1502 <50088704+santiagodelgado1502@users.noreply.github.com>
2019-05-04 20:19:58 -0500

    Add files via upload

---
+++ pila1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pila
+{
+    class pila1
+    {
+        Nodo Primero; 
+
+        public pila1()
+        {
+            Primero = null; 
+        }
+
+        public void InsertarElemento(char Dato)
+        {
+            if (Primero == null)
+            { 
+                Primero = new Nodo(Dato, null);
+            }
+            else
+            {
+                Nodo NuevoElemento = new Nodo(Dato, Primero);
+                Primero = NuevoElemento;
+            }
+        }
+        public void EliminarElemento()
+        {
+            if (Primero == null) { 
+                System.Console.WriteLine("Pila vacía");
+            }
+            else if (Primero.Siguiente == null)
+                Primero = null;
+
+            else
+                Primero = Primero.Siguiente;
+        }
+        public char ObtenerElemento(int Indice)
+        {
+            if (Primero == null)
+            {
+                System.Console.WriteLine("Pila vacía");
+                return null;
+            }
+
+            else
+            {
+                int Contador = 1;
+
+                Nodo NodoActual = Primero;
+
+                while (NodoActual is Nodo)
+                {
+                    if (Contador == Indice)
+                    { 
+                        return NodoActual.Valor;
+                    }
+                Contador++; 
+                    NodoActual = ActualNodo.Siguiente;
+                }
+
+                System.Console.WriteLine("No existe");
+                return null;
+            }
+
+        }
+}
+}
